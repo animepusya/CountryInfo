f6b0314f7abcb8c8d3c52a762d8355bf379f2f38
animerusya <rus.melanin@mail.ru>
2025-07-03 12:57:51 +0300

    Project added

---
+++ CountryInfo/CountryInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryInfoApp.swift
+//  CountryInfo
+//
+//  Created by Руслан Меланин on 03.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct CountryInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
